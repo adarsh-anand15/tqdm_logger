3dabbd3e4c5a2c12f302f5adf58674dd5f561320
Adarsh Anand <adarsh.anand15@gmail.com>
2021-05-25 02:24:57 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tqdm_logger',
+    scripts=['tqdm_logger.py'],
+    description='TQDM Logging utility',
+    version='1.0.0d',
+    url='https://github.com/adarsh-anand15/tqdm_logger',
+    author='Adarsh Anand',
+    author_email='adarsh.anand15@gmail.com',
+)
