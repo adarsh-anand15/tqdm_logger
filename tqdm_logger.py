c13e6b10d18a3dad917c50b37e47352931b76d06
Adarsh Anand <adarsh.a@karza.in>
2021-11-01 15:39:44 +0530

    some updates to show to stdout as well

--- tqdm_logger.py
+++ tqdm_logger.py
@@ -12,6 +12,7 @@ __url__          = 'https://github.com/adarsh-anand15/tqdm_logger'
 
 import io
 import os
+import sys
 
 __all__ = ['TqdmLogger']
 
@@ -23,7 +24,7 @@ class TqdmLogger(io.StringIO):
         It is necessary to reset the logger stream everytime before starting a new tqdm progress bar.
     """
 
-    def __init__(self, log_file):
+    def __init__(self, log_file, bar_stdout_flag = True):
         """
         Initialize with a log file to use for logging, 
         a buf to catch logs, and a bar index that stores the line number of the bar in file, 
@@ -38,6 +39,7 @@ class TqdmLogger(io.StringIO):
         self.logger = log_file
         self.buf = ''
         self.bar_index = -1
+        self.bar_stdout_flag = bar_stdout_flag
 
         # Create the log file if it doesn't already exists
         if not os.path.isfile(self.logger):
@@ -52,6 +54,7 @@ class TqdmLogger(io.StringIO):
         """
 
         self.bar_index = -1
+        self.buf = ''
         
     def write(self, buf, log_type = None):
         """
@@ -60,10 +63,15 @@ class TqdmLogger(io.StringIO):
         
         if buf.strip() and (log_type == 'text' or ('it/s' not in buf.strip() and 's/it' not in buf.strip())):
             with open(self.logger, 'a') as ofile:
-                ofile.write(buf.strip() + '\n')
+                ofile.write(buf)
                 ofile.flush()
+            sys.stdout.write(buf.rstrip() + '\n')
+            sys.stdout.flush()
         else:
             self.buf = buf.strip('\r\n\t ')
+            if self.bar_stdout_flag:
+                sys.stdout.write(buf)
+                sys.stdout.flush()
     
     def flush(self):
         """
@@ -74,6 +82,7 @@ class TqdmLogger(io.StringIO):
         #skip if buffer is empty
         if not self.buf.strip():
             return
+        sys.stdout.flush()
           
         # Read the entire content of the log file
         ifstream = open(self.logger, 'r')
@@ -81,11 +90,10 @@ class TqdmLogger(io.StringIO):
         ifstream.close()
         
         # Replace the progress bar line with new value
-        if self.bar_index == -1:
-            lines.append(self.buf.strip())
-            self.bar_index = len(lines) - 1
-        else:
-            lines[self.bar_index] = self.buf.strip()
+        if self.bar_index != -1:
+            del lines[self.bar_index]
+        lines.append(self.buf.strip())
+        self.bar_index = len(lines) - 1
             
         for i, line in enumerate(lines):
             lines[i] = line.strip() + '\n'
